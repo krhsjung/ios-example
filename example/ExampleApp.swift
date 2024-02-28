9d19c1a954bda942cb9188400ad13357b975f59e
krhsjung <161413218+krhsjung@users.noreply.github.com>
2024-02-28 15:27:06 +0900

    Initial commit

---
+++ example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by Hee Seok Jung on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ example/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  example
+//
+//  Created by Hee Seok Jung on 2/28/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
